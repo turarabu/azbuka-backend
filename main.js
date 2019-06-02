d852a56bee74b6c7d65e0c2e5c0645b8a982fbef
Turar Abu <erfalon@gmail.com>
2019-06-02 13:15:17 +0600

    body parser limit 100mb

--- main.js
+++ main.js
@@ -9,8 +9,8 @@ const database = require('./core/database');
 const app = express();
 const DB = database.init(config.database, MongoClient);
 
-app.use( express.json() );
-app.use( express.urlencoded({ extended: true }) );
+app.use( express.json({ limit: '100mb' }) );
+app.use( express.urlencoded({ extended: true, limit: '100mb' }) );
 
 app.listen(config.server.port, async function () {
     var db = await DB.catch(process.exit);
