553173f03b97568fb53bb7acc2cde3db8b08d3d1
Turar Abu <erfalon@gmail.com>
2019-07-22 19:39:38 +1000

    fifth 22.07.19

--- core/routes/cart.js
+++ core/routes/cart.js
@@ -21,7 +21,7 @@ function order (req, res) {
     var path = `/madein/hs/mobileapp/orders?${ qstr.stringify({id}) }`
     console.log(req.query)
     this.success()
-    // request(path, 'POST', JSON.parse(req.query.json), this.success)
+    request(path, 'POST', JSON.parse(req.query.json), this.success)
 }
 
 function request (path, method, post, callback) {
